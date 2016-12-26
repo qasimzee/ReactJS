fb62d1a35d6c20c2d67c1e3fe05ff6313235b556
Qasim Zeeshan <newbillgates@gmail.com>
2016-12-26 19:09:03 +0700

    adding state and two way binding

--- react-blogging-web/js/components/Body.js
+++ react-blogging-web/js/components/Body.js
@@ -1,10 +1,15 @@
 import React from "react"
 
 export default class Body extends React.Component {
+  handleChange(e) {
+    const text = e.target.value;
+    this.props.changeText(text);
+  }
   render() {
     return (
       <div>
-      {this.props.value}
+        <div>{this.props.text}</div>
+        <input onChange={this.handleChange.bind(this)} />
       </div>
       
     );
--- react-blogging-web/js/components/Layout.js
+++ react-blogging-web/js/components/Layout.js
@@ -2,14 +2,22 @@ import React from "react"
 import Body from "./Body"
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "Blogging website"
+    }
+    
+  }
+  changeText(text) {
+    this.setState({text});
+  }
   render() {
     return (
       <div>
         <h1>Logo</h1>
-        <Body value="Yahoo" />
-        
-      </div>
-      
+        <Body text={this.state.text} changeText={this.changeText.bind(this)}/>
+      </div>      
     );
   }
 }
