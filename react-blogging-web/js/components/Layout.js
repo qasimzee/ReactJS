2753b6ed4f97967f3b84395f5639203f2cf5a1e2
Qasim Zeeshan <newbillgates@gmail.com>
2016-12-26 18:22:36 +0700

    first commit

---
+++ react-blogging-web/js/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Body from "./Body"
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Logo</h1>
+        <Body value="Yahoo" />
+        
+      </div>
+      
+    );
+  }
+}
